aad3b6b0de4f49cd1ce6ff791d333e135c6b63f7
Milan <156816555+firearz@users.noreply.github.com>
2025-05-02 17:18:31 +0530

    Delete setup.bat

--- setup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing requirements...
-pip install -r requirements.txt
-
-echo Building launcher...
-pyinstaller --noconfirm --onefile --windowed launcher/main.py --icon=assets/logo.png
-
-echo Done. Executable should be in the dist\ folder.
-pause
